c7ef73910dce467783198455395858b4e302a0ba
Edinei <galera.org@gmail.com>
2018-04-26 17:49:39 -0300

    version fix changes config

--- view/inc/version_control.php
+++ view/inc/version_control.php
@@ -15,9 +15,9 @@ if(file_exists(PATH_HOME . "_config/updates/version.txt")) {
     $actual = file_get_contents(PATH_HOME . "composer.lock");
     if($old !== $actual) {
         $conf = file_get_contents(PATH_HOME . "_config/config.php");
-        $version = (float) explode(')', explode("'VERSION', ", $conf)[1])[0];
+        $version = (float) explode("')", explode("'VERSION', '", $conf)[1])[0];
         $newVersion = $version + 0.01;
-        $conf = str_replace("'VERSION', {$version})", "'VERSION', {$newVersion})", $conf);
+        $conf = str_replace("'VERSION', '{$version}')", "'VERSION', '{$newVersion}')", $conf);
         $f = fopen(PATH_HOME . "_config/config.php", "w");
         fwrite($f, $conf);
         fclose($f);
