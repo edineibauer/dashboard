12e7412efcd6c4e2ff435a04c38dc7bf6630e74d
Edinei <galera.org@gmail.com>
2018-05-03 19:22:23 -0300

    go

--- ajax/settings/saveConfig.php
+++ ajax/settings/saveConfig.php
@@ -5,6 +5,7 @@ $value = trim(strip_tags(filter_input(INPUT_POST, "value", FILTER_DEFAULT)));
 if(\Helpers\Check::isJson($value) && preg_match('/url\":/i', $value))
     $value = str_replace('\\', '/', json_decode($value, true)[0]['url']);
 
+
 $file = file_get_contents(PATH_HOME . "_config/config.php");
 if (preg_match("/\'{$field}\',/i", $file)) {
     $valueOld = explode("'", explode("('{$field}', '", $file)[1])[0];
