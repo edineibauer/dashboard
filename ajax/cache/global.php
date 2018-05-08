7842af3597b12e6eb1bef8b338387c6079cd8838
Edinei <galera.org@gmail.com>
2018-05-08 17:37:34 -0300

    remove theme min from delete cache

--- ajax/cache/global.php
+++ ajax/cache/global.php
@@ -3,7 +3,7 @@
 $dir = PATH_HOME . (DEV ? "assetsPublic" : "assets");
 foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
     RecursiveIteratorIterator::CHILD_FIRST) as $file) {
-    if(!in_array($file->getFileName(), ["theme.css", "theme"])) {
+    if(!in_array($file->getFileName(), ["theme.css", "theme.min.css", "theme"])) {
         if ($file->isDir())
             rmdir($file->getRealPath());
         elseif ($file->getFileName())
--- view/inc/version_control.php
+++ view/inc/version_control.php
@@ -9,7 +9,7 @@ function updateVersionTxt() {
     $dir = PATH_HOME . (DEV ? "assetsPublic" : "assets");
     foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
         RecursiveIteratorIterator::CHILD_FIRST) as $file) {
-        if(!in_array($file->getFileName(), ["theme.css", "theme"])) {
+        if(!in_array($file->getFileName(), ["theme.css", "theme.min.css", "theme"])) {
             if ($file->isDir())
                 rmdir($file->getRealPath());
             elseif ($file->getFileName())
