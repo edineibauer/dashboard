1ff68c68c28111787eb53621d0a364ca04e7b7d8
Edinei <galera.org@gmail.com>
2018-05-08 17:40:44 -0300

    update version when save theme

--- ajax/tema/saveTheme.php
+++ ajax/tema/saveTheme.php
@@ -15,3 +15,16 @@ $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '',
 $f = fopen(PATH_HOME . "{$assets}/theme/theme.min.css", "w+");
 fwrite($f, $buffer);
 fclose($f);
+
+$conf = file_get_contents(PATH_HOME . "_config/config.php");
+$version = explode("')", explode("'VERSION', '", $conf)[1])[0];
+$newVersion = $version + 0.01;
+$conf = str_replace("'VERSION', '{$version}')", "'VERSION', '{$newVersion}')", $conf);
+$f = fopen(PATH_HOME . "_config/config.php", "w");
+fwrite($f, $conf);
+fclose($f);
+updateVersionTxt();
+
+$f = fopen(PATH_HOME . "_config/updates/version.txt", "w+");
+fwrite($f, file_get_contents(PATH_HOME . "composer.lock"));
+fclose($f);
