45fd1364d49306d1c55b97916043778edff8ddaa
Edinei <galera.org@gmail.com>
2018-05-10 14:07:22 -0300

    js

--- assets/social_connect.js
+++ assets/social_connect.js
@@ -1,12 +1,26 @@
 function saveSocialSettings(field, value) {
     post("dashboard", "settings/saveConfig", {field: field, value: value}, function (g) {
         if (g)
-            toast("erro", "warning");
-    });
+            toast("erro", "warning")
+    })
+}
+
+function checkIfButtonConnectExist(id) {
+    if (!$(id).length) {
+        post("dashboard", "social_connect/checkButtonConnect", {id: id}, function (g) {
+            if (g)
+                $("#space-" + id.substr(1,id.length)).html(g);
+        });
+    }
 }
 
 $(function () {
     $(".inputConfig").off("keyup change").on("keyup change", function () {
+        if ($(this).val().length === 0)
+            $("#" + $(this).attr("rel")).remove();
+        else
+            checkIfButtonConnectExist("#" + $(this).attr("rel"));
+
         saveSocialSettings($(this).attr("id"), $(this).val());
     });
-});
+})
