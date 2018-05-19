019800371c455b6dcc1c337d011ce876d1fae1bf
Edinei <galera.org@gmail.com>
2018-05-18 21:43:14 -0300

    create manifest if not exist in save config dashboard

--- ajax/settings/saveConfig.php
+++ ajax/settings/saveConfig.php
@@ -25,7 +25,28 @@ if (\Helpers\Check::isJson($value) && preg_match('/url\":/i', $value)) {
 
     if(strlen($value) < 7 && strlen($value) > 20)
         $value = "";
+} elseif ($field === "FAVICON") {
+    $fav = \WideImage\WideImage::load(PATH_HOME . $value);
+    $fav->resize(256, 256)->saveToFile(PATH_HOME . "uploads/site/favicon-256.png");
+    $fav->resize(192, 192)->saveToFile(PATH_HOME . "uploads/site/favicon-192.png");
+    $fav->resize(152, 152)->saveToFile(PATH_HOME . "uploads/site/favicon-152.png");
+    $fav->resize(144, 144)->saveToFile(PATH_HOME . "uploads/site/favicon-144.png");
+    $fav->resize(128, 128)->saveToFile(PATH_HOME . "uploads/site/favicon-128.png");
+    $fav->resize(90, 90)->saveToFile(PATH_HOME . "uploads/site/favicon-90.png");
+    $value = "uploads/site/favicon-90.png";
 }
+
+if(!file_exists(PATH_HOME . "manifest.json")) {
+    $theme = explode("}", explode(".theme {", file_get_contents(PATH_HOME . "assets" . (DEV ? "Public" : "") . "/theme/theme.css"))[1])[0];
+    $themeBack = explode(" ", explode("background-color:", $theme)[1])[0];
+    $themeColor = explode(" ", explode("color:", $theme)[1])[0];
+    $content = str_replace(['{$sitename}', '{$favicon}', '{$theme}', '{$themeColor}'], [SITENAME, FAVICON, $themeBack, $themeColor], file_get_contents(PATH_HOME . "vendor/conn/config/tpl/manifest.txt"));
+
+    $fp = fopen(PATH_HOME . "manifest.json", "w+");
+    fwrite($fp, $content);
+    fclose($fp);
+}
+
 $value = trim(strip_tags(str_replace("'", '"', $value)));
 
 $file = file_get_contents(PATH_HOME . "_config/config.php");
