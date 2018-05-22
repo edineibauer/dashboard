a69b4488485713ba30f3a11fdfd02d5c7a3537ac
Edinei <galera.org@gmail.com>
2018-05-21 23:11:46 -0300

    up

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,24 +1,6 @@
-var mySidebar = document.getElementById("mySidebar");
-var overlayBg = document.getElementById("myOverlay");
-
 function logoutDashboard() {
     if (confirm("Desconectar?"))
         window.location = HOME + "logout";
-}
-
-function open_sidebar() {
-    if (mySidebar.style.display === 'block') {
-        mySidebar.style.display = 'none';
-        overlayBg.style.display = "none";
-    } else {
-        mySidebar.style.display = 'block';
-        overlayBg.style.display = "block";
-    }
-}
-
-function close_sidebar() {
-    mySidebar.style.display = "none";
-    overlayBg.style.display = "none";
 }
 
 function hide_sidebar_small() {
@@ -27,7 +9,7 @@ function hide_sidebar_small() {
 }
 
 $(function () {
-    $(".menu-li").off("click").on("click", function () {
+    $("#content").off("click", ".menu-li").on("click", ".menu-li", function () {
         var lib = $(this).attr("data-lib");
         var attr = $(this).attr("data-atributo");
         $(".main").loading();
@@ -42,15 +24,26 @@ $(function () {
                 $("#dashboard").html(data);
             });
         }
-    });
 
-    $("#btn-editLogin").off("click").on("click", function () {
+    }).off("click", "#btn-editLogin").on("click", "#btn-editLogin", function () {
         $(this).panel(themeWindow("Editar Perfil", {lib: 'dashboard', file: 'edit/perfil'}, function (idOntab) {
             data = formGetData($("#" + idOntab).find(".ontab-content").find(".form-crud"));
             post('dashboard', 'edit/session', {dados: data}, function () {
                 location.reload();
             });
         }));
+
+    });
+    $(".open-menu").off("click").on("click", function () {
+        if ($("#myOverlay").css("display") === 'block') {
+            $("#myOverlay, #mySidebar").css("display", "none");
+        } else {
+            $("#myOverlay, #mySidebar").css("display", "block");
+
+            $("#myOverlay").off("click").on("click", function () {
+                $("#myOverlay, #mySidebar").css("display", "none");
+            });
+        }
     });
 
     post("dashboard", "dash/geral", {}, function (data) {
