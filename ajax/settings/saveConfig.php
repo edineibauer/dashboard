9b4e88a444f7c640623a848058c36f8e7c710c86
Edinei <galera.org@gmail.com>
2018-05-28 10:43:42 -0300

    save theme right manifest

--- ajax/settings/saveConfig.php
+++ ajax/settings/saveConfig.php
@@ -38,9 +38,9 @@ if (\Helpers\Check::isJson($value) && preg_match('/url\":/i', $value)) {
 
 /** MANIFEST UPDATE CREATE */
 function createManifest() {
-    $theme = explode("}", explode(".theme {", file_get_contents(PATH_HOME . "assets" . (DEV ? "Public" : "") . "/theme/theme.css"))[1])[0];
-    $themeBack = explode(" ", explode("background-color:", $theme)[1])[0];
-    $themeColor = explode(" ", explode("color:", $theme)[1])[0];
+    $theme = explode("}", explode(".theme{", file_get_contents(PATH_HOME . "assets" . (DEV ? "Public" : "") . "/theme/theme.css"))[1])[0];
+    $themeBack = explode("!important", explode("background-color:", $theme)[1])[0];
+    $themeColor = explode("!important", explode("color:", $theme)[1])[0];
     $content = str_replace(['{$sitename}', '{$favicon}', '{$theme}', '{$themeColor}'], [SITENAME, FAVICON, $themeBack, $themeColor], file_get_contents(PATH_HOME . "vendor/conn/config/tpl/manifest.txt"));
 
     $fp = fopen(PATH_HOME . "manifest.json", "w+");
