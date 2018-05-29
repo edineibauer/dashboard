bd94134d2e0ab699fff005cb8e07047359ecb1e8
Edinei <galera.org@gmail.com>
2018-05-29 19:00:21 -0300

    dont delete global

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,41 +1,43 @@
+function logoutDashboard() {
+    if (confirm("Desconectar?"))
+        window.location = HOME + "logout"
+}
+
 function hide_sidebar_small() {
-    if (screen.width < 993)
-        close_sidebar();
+    if (screen.width < 993) {
+        $("#myOverlay, #mySidebar").css("display", "none");
+    }
 }
 
 $(function () {
-    $("#content").off("click", ".menu-li").on("click", ".menu-li", function () {
-        $("#content .menu-li").removeClass("color-grey-light");
-        $(this).addClass("color-grey-light");
+    $("#content, #app-sidebar").off("click", ".menu-li").on("click", ".menu-li", function () {
         var lib = $(this).attr("data-lib");
         var attr = $(this).attr("data-atributo");
         $(".main").loading();
         hide_sidebar_small();
-
-        if(lib === "") {
+        closeSidebar();
+        if (lib === "") {
             post("table", "api", {entity: attr}, function (data) {
-                $("#dashboard").html(data);
-                spaceHeader();
-            });
+                $("#dashboard").html(data)
+            })
         } else {
             get(attr, function (data) {
-                $("#dashboard").html(data.content);
-                spaceHeader();
-            });
+                $("#dashboard").html(data.content)
+            })
         }
-
     }).off("click", "#btn-editLogin").on("click", "#btn-editLogin", function () {
         $(this).panel(themeWindow("Editar Perfil", {lib: 'dashboard', file: 'edit/perfil'}, function (idOntab) {
             data = formGetData($("#" + idOntab).find(".ontab-content").find(".form-crud"));
             post('dashboard', 'edit/session', {dados: data}, function () {
-                location.reload();
-            });
-        }));
-
+                location.reload()
+            })
+        }))
     });
 
-    get("dash/geral", function (data) {
-        $("#dashboard").html(data);
-        spaceHeader();
-    });
-});
+    setTimeout(function () {
+        get("dash/geral", function (data) {
+            $("#dashboard").html(data.content);
+            spaceHeader();
+        })
+    },300);
+})
