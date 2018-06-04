3b121e3f7c9308089caf8c6f8713e355c3b58539
Edinei <galera.org@gmail.com>
2018-06-04 13:41:14 -0300

    menus personalizados por setor

--- ajax/view/dashboard.php
+++ ajax/view/dashboard.php
@@ -5,43 +5,55 @@ if (!LOGGED) {
 } else {
     ob_start();
     include_once 'inc/version_control.php';
-    if($data['response'] === 1) {
-        ?>
-        <nav class="sidebar card collapse color-white animate-left dashboard-nav space-header" id="mySidebar"><br>
-            <div class="container row">
-                <?php
-                if (isset($_SESSION['userlogin']['imagem']) && !empty($_SESSION['userlogin']['imagem'])) {
-                    echo '<div class="col s4"><img src="' . $_SESSION['userlogin']['imagem'] . '" style="margin-bottom:0!important" class="card radius-small margin-right"></div><div class="col s8 bar">';
-                } else {
-                    echo '<div class="col s12 bar">';
-                }
-                ?>
+    ?>
+    <nav class="sidebar card collapse color-white animate-left dashboard-nav space-header" id="mySidebar"><br>
+        <div class="container row">
+            <?php
+            if (isset($_SESSION['userlogin']['imagem']) && !empty($_SESSION['userlogin']['imagem'])) {
+                echo '<div class="col s4"><img src="' . $_SESSION['userlogin']['imagem'] . '" style="margin-bottom:0!important" class="card radius-small margin-right"></div><div class="col s8 bar">';
+            } else {
+                echo '<div class="col s12 bar">';
+            }
+            ?>
 
-                <strong class="padding"><?= $_SESSION['userlogin']['nome'] ?></strong><br>
-                <div class="row padding-medium div-btn-editLogin">
-                    <button id="btn-editLogin"
-                            class="left color-white opacity hover-shadow hover-opacity-off btn color-grey-light margin-0">
-                        <i class="material-icons left font-large">edit</i>
-                        <span class="left">editar</span>
-                    </button>
-                </div>
+            <strong class="padding"><?= $_SESSION['userlogin']['nome'] ?></strong><br>
+            <div class="row padding-medium div-btn-editLogin">
+                <button id="btn-editLogin"
+                        class="left color-white opacity hover-shadow hover-opacity-off btn color-grey-light margin-0">
+                    <i class="material-icons left font-large">edit</i>
+                    <span class="left">editar</span>
+                </button>
             </div>
-            </div>
-            <hr style="margin: 30px 0 0;">
-            <div class="bar-block">
-                <?php
-                require_once 'inc/menu.php';
-                ?>
-                <br><br>
-            </div>
-        </nav>
-
-        <div class="main dashboard-main">
-            <div id="dashboard" class="dashboard-tab container row"></div>
         </div>
+        </div>
+        <hr style="margin: 30px 0 0;">
+        <div class="bar-block">
+            <?php
+            $inc = false;
+            foreach (\Helpers\Helper::listFolder(PATH_HOME . "vendor/conn") as $lib) {
+                if (!$inc && file_exists(PATH_HOME . "vendor/conn/{$lib}/ajax/view/inc/menu-{$_SESSION['userlogin']['setor']}.php")) {
+                    $inc = true;
+                    require_once PATH_HOME . "vendor/conn/{$lib}/ajax/view/inc/menu-{$_SESSION['userlogin']['setor']}.php";
+                }
+            }
 
-        <?php
-        $data['data']['content'] = ob_get_contents();
-    }
+            if(!$inc && DEV && file_exists(PATH_HOME . "ajax/view/inc/menu-{$_SESSION['userlogin']['setor']}.php")){
+                $inc = true;
+                require_once PATH_HOME . "ajax/view/inc/menu-{$_SESSION['userlogin']['setor']}.php";
+            }
+
+            if (!$inc)
+                require_once 'inc/menu.php';
+            ?>
+            <br><br>
+        </div>
+    </nav>
+
+    <div class="main dashboard-main">
+        <div id="dashboard" class="dashboard-tab container row"></div>
+    </div>
+
+    <?php
+    $data['data']['content'] = ob_get_contents();
     ob_end_clean();
 }
