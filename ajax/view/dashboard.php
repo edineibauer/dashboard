95c2c06959e21daccfb3a4f767ae23698a4ccd41
Edinei <galera.org@gmail.com>
2018-06-05 09:26:40 -0300

    image perfil dashboard updated

--- ajax/view/dashboard.php
+++ ajax/view/dashboard.php
@@ -10,23 +10,24 @@ if (!LOGGED) {
         <div class="container row">
             <?php
             if (isset($_SESSION['userlogin']['imagem']) && !empty($_SESSION['userlogin']['imagem'])) {
-                echo '<div class="col s4"><img src="' . $_SESSION['userlogin']['imagem'] . '" style="margin-bottom:0!important" class="card radius-small margin-right"></div><div class="col s8 bar">';
+                echo '<div class="col s4"><img src="'. HOME . 'image/' . $_SESSION['userlogin']['imagem'] . '&w=80&h=80" width="72" height="72" style="margin-bottom:0!important; width: 72px;height: 72px" class="card radius-circle margin-right"></div>';
             } else {
-                echo '<div class="col s12 bar">';
+                echo '<div class="col s4"><i class="material-icons font-jumbo">people</i></div>';
             }
             ?>
 
-            <strong class="padding"><?= $_SESSION['userlogin']['nome'] ?></strong><br>
-            <div class="row padding-medium div-btn-editLogin">
-                <button id="btn-editLogin"
-                        class="left color-white opacity hover-shadow hover-opacity-off btn color-grey-light margin-0">
-                    <i class="material-icons left font-large">edit</i>
-                    <span class="left">editar</span>
-                </button>
+            <div class="col s8 bar">
+                <strong class="padding"><?= $_SESSION['userlogin']['nome'] ?></strong><br>
+                <div class="row padding-medium div-btn-editLogin">
+                    <button id="btn-editLogin"
+                            class="left color-white opacity hover-shadow hover-opacity-off btn color-grey-light margin-0">
+                        <i class="material-icons left font-large">edit</i>
+                        <span class="left">editar</span>
+                    </button>
+                </div>
             </div>
         </div>
-        </div>
-        <hr style="margin: 30px 0 0;">
+        <hr style="margin: 15px 0 0;">
         <div class="bar-block">
             <?php
             $inc = false;
