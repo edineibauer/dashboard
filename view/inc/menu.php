50bd6a7566800487326b1cd3269c082025f0d90c
Edinei <galera.org@gmail.com>
2018-07-26 11:59:45 -0300

    updates dash control

--- view/inc/menu.php
+++ view/inc/menu.php
@@ -1,25 +1,42 @@
 <?php
 
-function checkFolder(string $dir, bool $inc) {
-    if($inc)
-        return true;
-
+/**
+ * @param string $dir
+ * @return bool
+ */
+function checkFolder(string $dir): bool
+{
     if (file_exists($dir)) {
         require_once $dir;
         return true;
     }
-
     return false;
 }
 
 $inc = false;
-foreach (\Helpers\Helper::listFolder(PATH_HOME . "vendor/conn") as $lib)
-    $inc = checkFolder(PATH_HOME . VENDOR . "{$lib}/dash/{$_SESSION['userlogin']['setor']}/menu.php", $inc);
 
-if(DEV)
-    $inc = checkFolder(PATH_HOME . "dash/{$_SESSION['userlogin']['setor']}/menu.php", $inc);
+//Menu Personalizado
+$inc = checkFolder(PATH_HOME . "dash/{$_SESSION['userlogin']['setor']}/menu.php");
+if (!$inc) {
+    foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR) as $lib) {
+        if (!$inc)
+            $inc = checkFolder(PATH_HOME . VENDOR . "{$lib}/dash/{$_SESSION['userlogin']['setor']}/menu.php");
+    }
 
-if(!$inc) {
-    $menu = new \Dashboard\Menu();
-    echo $menu->getMenu();
+    if (!$inc) {
+        //Menu Personalizado Genérico
+        $inc = checkFolder(PATH_HOME . "dash/0/menu.php");
+        if (!$inc) {
+            foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR) as $lib) {
+                if (!$inc)
+                    $inc = checkFolder(PATH_HOME . VENDOR . "{$lib}/dash/0/menu.php");
+            }
+
+            //Menu Entity Genérico
+            if (!$inc) {
+                $menu = new \Dashboard\Menu();
+                echo $menu->getMenu();
+            }
+        }
+    }
 }
