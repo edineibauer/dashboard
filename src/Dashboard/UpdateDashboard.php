6a27baa43f699c068170e1eb2ec05821c8a6a4df
Edinei <galera.org@gmail.com>
2018-10-05 21:04:13 -0300

    not minify assets libs on update system

--- src/Dashboard/UpdateDashboard.php
+++ src/Dashboard/UpdateDashboard.php
@@ -76,7 +76,6 @@ class UpdateDashboard
         $this->updateDependenciesEntity();
         $this->checkAdminExist();
         $this->updateAssets();
-        $this->createMinifyAssetsLib();
         $this->updateServiceWorker($version);
         $this->result = true;
     }
@@ -208,26 +207,6 @@ class UpdateDashboard
         }
 
         return [$listShell, $listData];
-    }
-
-    private function createMinifyAssetsLib()
-    {
-        //Para cada arquivo css e js presente nas bibliotecas dentro da pasta assets, minifica quando não existe
-        foreach (Helper::listFolder(PATH_HOME . VENDOR) as $lib) {
-            foreach (Helper::listFolder(PATH_HOME . VENDOR . $lib . "/assets") as $file) {
-                $ext = pathinfo($file, PATHINFO_EXTENSION);
-                $name = pathinfo($file, PATHINFO_FILENAME);
-
-                if (in_array($ext, ['css', 'js']) && !file_exists(PATH_HOME . VENDOR . $lib . "/assets/{$name}.min.{$ext}") && !preg_match('/\w+\.min\.(css|js)$/i', $name)) {
-                    if ($ext === "js")
-                        $minifier = new Minify\JS(file_get_contents(PATH_HOME . VENDOR . $lib . "/assets/{$name}.js"));
-                    else
-                        $minifier = new Minify\CSS(file_get_contents(PATH_HOME . VENDOR . $lib . "/assets/{$name}.css"));
-
-                    $minifier->minify(PATH_HOME . VENDOR . $lib . "/assets/{$name}.min.{$ext}");
-                }
-            }
-        }
     }
 
     /**
