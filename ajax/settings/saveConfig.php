3e4deb973112e2adfc97e241f64811a07de53f7a
Edinei <galera.org@gmail.com>
2018-10-09 16:24:28 -0300

    save configs

--- ajax/settings/saveConfig.php
+++ ajax/settings/saveConfig.php
@@ -5,3 +5,5 @@ $value = trim(strip_tags(filter_input(INPUT_POST, "value", FILTER_DEFAULT)));
 //Abre json config and add/update field
 $config = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
 $config[$field] = str_replace("'", '"', $value);
+
+Config\Config::createConfig($config);
