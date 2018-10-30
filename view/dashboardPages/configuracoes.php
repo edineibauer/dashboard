9ac6d43152be4b17404fa29e7e517f11c8c58a13
Edinei <galera.org@gmail.com>
2018-10-30 11:57:15 -0300

    integrações novo padrão separado das configurações do site

--- view/dashboardPages/configuracoes.php
+++ view/dashboardPages/configuracoes.php
@@ -1,27 +1,11 @@
 <?php
 $tpl = new \Helpers\Template("dashboard");
 $read = new \ConnCrud\Read();
-$dados['dominio'] = DOMINIO === "dashboard" ? "" : VENDOR . "dashboard/";
-$dados['version'] = VERSION;
-$dados['config'] = [
-    "sitename" => SITENAME,
-    "sitesub" => SITESUB,
-    "sitedesc" => SITEDESC,
-    "logo" => LOGO,
-    "analytics" =>  defined('ANALYTICS') && !empty(ANALYTICS) ? ANALYTICS : "",
-    "favicon" => FAVICON,
-    "protocol" => PROTOCOL,
-    "cepaberto" => defined('CEPABERTO') && !empty(CEPABERTO) ? CEPABERTO : "",
-    "spacekey" => defined('SPACEKEY') && !empty(SPACEKEY) ? SPACEKEY : "",
-    "recaptchasite" => defined('RECAPTCHASITE') && !empty(RECAPTCHASITE) ? RECAPTCHASITE : "",
-    "recaptcha" => defined('RECAPTCHA') && !empty(RECAPTCHA) ? RECAPTCHA : "",
-    "email" => defined('EMAIL') && !empty(EMAIL) ? EMAIL : "",
-    "emailkey" => defined('EMAILKEY') && !empty(EMAILKEY) ? EMAILKEY : "",
-];
 
 //Garante criação dos dados de configuração caso não exista
 $read->exeRead(PRE . "config", "WHERE id=1");
 if (!$read->getResult()) {
+
     $criarData = [
         "nome_do_site" => defined('SITENAME') && !empty(SITENAME) ? SITENAME : "",
         "subtitulo" => defined('SITESUB') && !empty(SITESUB) ? SITESUB : "",
@@ -31,6 +15,7 @@ if (!$read->getResult()) {
         "logo" => defined('LOGO') && !empty(LOGO) ? '[{"url": "' . LOGO . '", "name": "", "size": 1078, "type": "image/png"}]' : null,
         "favicon" => defined('FAVICON') && !empty(FAVICON) ? '[{"url": "' . FAVICON . '", "name": "", "size": 1078, "type": "image/png"}]' : null,
     ];
+
     $d = new \EntityForm\Dicionario("config");
     $d->setData($criarData);
     $d->save();
@@ -39,4 +24,4 @@ if (!$read->getResult()) {
 $form = new \FormCrud\Form("config");
 $dados['configForm'] = $form->getForm(1);
 
-$data['data'] = $tpl->getShow('settings', $dados);
+$data['data'] = $tpl->getShow('configuracoes', $dados);
---
+++ view/dashboardPages/integracoes.php
@@ -0,0 +1,21 @@
+<?php
+$tpl = new \Helpers\Template("dashboard");
+$integ = ['integration' => []];
+$dados = [];
+
+if(file_exists(PATH_HOME . "_config/config.json"))
+    $dados = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
+
+foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR) as $item) {
+    if(file_exists(PATH_HOME . VENDOR . $item . "/constante")){
+        foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR . $item . "/constante") as $c) {
+            $file = json_decode(file_get_contents(PATH_HOME . VENDOR . $item . "/constante/{$c}"), true);
+            foreach ($file['constantes'] as $nome => $column)
+                $file['constantes'][$nome]['value'] = $dados[$file['constantes'][$nome]['column']] ?? "";
+
+            $integ['integration'][] = $file;
+        }
+    }
+}
+
+$data['data'] = $tpl->getShow('integracoes', $integ);
