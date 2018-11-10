d1b914f55275f81ef117bd86a2c4779bfaaa0551
Edinei <galera.org@gmail.com>
2018-11-10 01:11:17 -0200

    recebe parametro force para forçar atualização do sistema, usado na inicialização da instalação

--- public/view/dashboardUpdateSystem.php
+++ public/view/dashboardUpdateSystem.php
@@ -1,4 +1,9 @@
 <?php
+
+$force = filter_input(INPUT_GET, 'force', FILTER_VALIDATE_INT);
+if(!empty($force) && $force === 1 && file_exists(PATH_HOME . "_config/updates/version.txt"))
+    unlink(PATH_HOME . "_config/updates/version.txt");
+
 $up = new \Dashboard\UpdateDashboard();
 
 $data['response'] = 3;
