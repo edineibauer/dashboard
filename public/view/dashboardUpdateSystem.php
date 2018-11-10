85d2e4172ec74e697fa22637c74f2ce1866de7c3
Edinei <galera.org@gmail.com>
2018-11-10 01:31:10 -0200

    force update first parameter extra

--- public/view/dashboardUpdateSystem.php
+++ public/view/dashboardUpdateSystem.php
@@ -1,8 +1,9 @@
 <?php
-
-$force = filter_input(INPUT_GET, 'force', FILTER_VALIDATE_INT);
-if(!empty($force) && $force === 1 && file_exists(PATH_HOME . "_config/updates/version.txt"))
-    unlink(PATH_HOME . "_config/updates/version.txt");
+if(!empty($route->getVar())) {
+    $force = $route->getVar()[0];
+    if ($force === "force" && file_exists(PATH_HOME . "_config/updates/version.txt"))
+        unlink(PATH_HOME . "_config/updates/version.txt");
+}
 
 $up = new \Dashboard\UpdateDashboard();
 
